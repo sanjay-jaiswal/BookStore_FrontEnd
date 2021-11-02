f7c93200afac74b444c316de6ce94877d0235c71
sanjay-jaiswal <sanjujaiswal.dev@gmail.com>
2021-11-02 09:33:00 +0530

    [Sanju] Added : Modifyed book display page

--- src/app/Components/displaybooks/displaybooks.component.ts
+++ src/app/Components/displaybooks/displaybooks.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BookService } from 'src/app/Services/book/book.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-displaybooks',
@@ -15,8 +16,11 @@ export class DisplaybooksComponent implements OnInit {
 
   bookArray:any
   totalBooks:any
+  ViewBook:any
+  BookId:any
+  storeResult:any
 
-  constructor(private bookservice:BookService) { }
+  constructor(private bookservice:BookService, private router:Router) { }
 
   ngOnInit(): void {
     this.FetchAllBooks();
@@ -36,8 +40,38 @@ export class DisplaybooksComponent implements OnInit {
       this.bookArray=response.result;
       this.totalBooks = response.result.length;
 
+      this.BookId = localStorage.getItem('BookId');
+      this.ShowBookDetails();
+
     })
   }
 
-  
+  // ClickOnBookToViewDetails(_id:any){
+  //   this.bookservice.FetchAllBooksData().subscribe((response:any)=>{
+  //     console.log(response.result);
+  //     this.ViewBook=response.result;
+
+  //     this.router.navigateByUrl('/home/description')
+  //   })
+  // }
+
+  ShowBookDetails() {
+    this.bookservice.FetchAllBooksData().subscribe((res: any) => {
+      res.result.forEach((storeBook: any) => {
+        if (storeBook._id == this.BookId) {
+          this.storeResult = storeBook;
+        }
+      });
+    })
+  }
+
+
+  DisplayBookDetails(book:any){
+    localStorage.setItem('BookId',book._id);
+    this.router.navigateByUrl('/home/description')
+
+  }
+
+
+
 }
